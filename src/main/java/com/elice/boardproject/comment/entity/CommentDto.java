6204ae7d3f4a4c0273778cb4f281891f5ddc47bf
Derit7042 <junsu6752@gmail.com>
2024-02-28 17:45:37 +0900

    comment develop

---
+++ src/main/java/com/elice/boardproject/comment/entity/CommentDto.java
@@ -0,0 +1,14 @@
+package com.elice.boardproject.comment.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDto {
+    private String content;
+}
