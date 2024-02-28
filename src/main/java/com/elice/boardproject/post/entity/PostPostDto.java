f4b65c8bb6b59ff62a4f8c2f1072675d7d489362
Derit7042 <junsu6752@gmail.com>
2024-02-28 17:43:01 +0900

    post develop

---
+++ src/main/java/com/elice/boardproject/post/entity/PostPostDto.java
@@ -0,0 +1,15 @@
+package com.elice.boardproject.post.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostPostDto {
+    private String title;
+    private String content;
+}
